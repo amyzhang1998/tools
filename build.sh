9f5ccd47f8ce1f3f86473657f9b572287fd01e89
littledian <1197434548@qq.com>
2020-01-01 02:14:53 +0800

    fix: update build shell

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 timestamp=`date "+%Y%m%d%H%M%S"`
-tag="littledian/tools-$timestamp"
+tag="littledian/tools:$timestamp"
 
 docker build -t "$tag" .
 
 have=$(docker inspect --format='{{.Name}}' $(docker ps -aq) |grep tools  | cut -d"/" -f2)
-if ($have) then
+if [[ $have == "tools" ]]; then
   docker container stop tools
   docker container rm tools
 fi
 
 tags=$(docker images | grep littledian/tool | awk '{print $2}')
-for item in tags
+for item in $tags
 do
-  if ("$tag"!=$item) then
-        docker rmi $item
+  if [[ $tag != $item ]]; then
+        docker rmi littledian/tools:$item
   fi
 done
 
